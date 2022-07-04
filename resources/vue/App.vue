ef6f598d6fb96c8f72c7e3ccb227a437ba58c210
micioner2 <mauriciozpdeveloper@gmail.com>
2022-07-04 14:21:50 -0500

    first commit

---
+++ resources/vue/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="">
+        <h1>{{mensaje}}</h1>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const mensaje = ref("Mauricio Zapata Pella");
+
+</script>
